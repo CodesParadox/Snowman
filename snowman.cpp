a4dba6ef3ccecb6d0a45eb3aa35bb099c750f825
unknown <galbraimok@hotmail.com>
2021-03-10 20:00:24 +0200

    done

---
+++ snowman.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "snowman.hpp"
+#include <array>
+#include <exception>
+
+using namespace std;
+
+using namespace ariel;
+
+namespace ariel
+{
+
+	std::string snowman(int x)
+	{
+		return "LOOK AT THAT SNOWMAN";
+	}
+}
+
+// int main(){
+
+// 	cout << "Pick a Hat" << endl;
+
+// }
